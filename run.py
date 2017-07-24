0a4cebef29b59bafb4fc55dc4910fafaa08f990d
NekodRider <driver2223@gmail.com>
2017-07-24 15:47:55 +0800

    test

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Sanic()
 
 @app.route("/api/signup/submit")
 async def submit(request):
-
+    return json({"hello":"world"})
 
 if __name=__=="__main__":
     app.run(host="0.0.0.0",port=8000)
