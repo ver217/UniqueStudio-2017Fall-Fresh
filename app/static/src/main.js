2d8839ff71e01f6a61a5439be7ed0c1fa0769c2e
NekodRider <driver2223@gmail.com>
2017-07-29 22:15:19 +0800

    fixing.

--- app/static/src/main.js
+++ app/static/src/main.js
@@ -2,6 +2,7 @@ function input_check() {
     return $('#password').val&&$('#id').val
 }
 function init() {
+    var info_table=$('#info_table');
     $('.ui.accordion').accordion();
     $('table').tablesort();
     $('#pick-out-btn').children().get().forEach(function (element) {
@@ -17,7 +18,7 @@ function judge(line,team) {
         line.transiton('fly left');
     }
 }
-var info_table=$('#info_table');
+
 function pick_out(event) {
     var lines=info_table.children('tbody').children('tr').get(),
         team=event.data.team;
