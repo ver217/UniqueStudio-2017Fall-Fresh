c867fa223c177514fbbdd06a26aa3e1b51e1cd9e
NekodRider <driver2223@gmail.com>
2017-07-29 21:18:41 +0800

    add pick_out btn

--- app/static/src/main.js
+++ app/static/src/main.js
@@ -4,17 +4,18 @@ function input_check() {
 function init() {
     $('.ui.accordion').accordion();
     $('table').tablesort();
+    $('#pick-out-btn').children.bind("click",{team:this.innerHTML},pick_out(event.data.team));
 }
-function judge(line) {
-    var line_team = line.getElementsByName(td)[-4];
+function judge(line,team) {
+    var line_team = line.getElementsByName('td')[-4];
     if(line_team===team){
 
     }else {
-        line.transiton('toggle');
+        line.transiton('fly left');
     }
 }
 var info_table=$('#info_table');
 function pick_out(team) {
-    var lines=info_table.getElementsByName(tr);
-    lines.map(judge);
+    var lines=info_table.getElementsByName('tr');
+    lines.map(judge(team));
 }
