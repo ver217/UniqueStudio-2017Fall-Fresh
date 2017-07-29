6f200f9f7ac258b1c823623f8f90237f52e57450
NekodRider <driver2223@gmail.com>
2017-07-29 20:27:41 +0800

    adjust table layout and add pick_out func

--- app/static/src/main.js
+++ app/static/src/main.js
@@ -5,3 +5,16 @@ function init() {
     $('.ui.accordion').accordion();
     $('table').tablesort();
 }
+function judge(line) {
+    var line_team = line.getElementsByName(td)[-4];
+    if(line_team===team){
+
+    }else {
+        line.transiton('toggle');
+    }
+}
+var info_table=$('#info_table');
+function pick_out(team) {
+    var lines=info_table.getElementsByName(tr);
+    lines.map(judge);
+}
