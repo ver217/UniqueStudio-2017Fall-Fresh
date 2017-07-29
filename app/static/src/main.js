9a6a9c964d7182915ecacb98608ffc10066e221b
NekodRider <driver2223@gmail.com>
2017-07-29 22:01:21 +0800

    fix pick_out but still WIP

--- app/static/src/main.js
+++ app/static/src/main.js
@@ -4,7 +4,9 @@ function input_check() {
 function init() {
     $('.ui.accordion').accordion();
     $('table').tablesort();
-    $('#pick-out-btn').children.bind("click",{team:this.innerHTML},pick_out(event.data.team));
+    $('#pick-out-btn').children().get().forEach(function (element) {
+        $(element).bind("click",{team:element.innerHTML},pick_out);
+    });
 }
 function judge(line,team) {
     var line_team = line.getElementsByName('td')[-4];
@@ -15,7 +17,8 @@ function judge(line,team) {
     }
 }
 var info_table=$('#info_table');
-function pick_out(team) {
-    var lines=info_table.getElementsByName('tr');
+function pick_out(event) {
+    var lines=info_table.children('tr').get(),
+        team=event.data.team;
     lines.map(judge(team));
 }
